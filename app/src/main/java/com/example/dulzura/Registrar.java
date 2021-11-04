aad9e4454edf67d052326d0ff19b351371d17378
Valentin <rubenvale2601@gmail.com>
2021-11-03 23:13:34 -0600

    se agrega base de datos

--- app/src/main/java/com/example/dulzura/Registrar.java
+++ app/src/main/java/com/example/dulzura/Registrar.java
@@ -1,20 +1,83 @@
 package com.example.dulzura;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registrar extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private EditText correo;
+
+    private EditText contraseña;
+
+    private EditText contraseñaConfirmacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+
+        mAuth = FirebaseAuth.getInstance();
+        correo = findViewById(R.id.correo);
+        contraseña = findViewById(R.id.contraseña);
+        contraseñaConfirmacion = findViewById(R.id.contraseñaConfirmacion);
     }
     public void irAInicio(View view){
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void registrarUsuario(View view){
+
+        if(contraseña.getText().toString().equals(contraseñaConfirmacion.getText().toString())){
+            mAuth.createUserWithEmailAndPassword(correo.getText().toString(), contraseña.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                //Log.d(TAG, "createUserWithEmail:success");
+                                Toast.makeText(getApplicationContext(), "usuario creado",Toast.LENGTH_SHORT).show();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(i);
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "Authentication failed.",Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+
+
+
+        }else{
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+        }
     }
 }
